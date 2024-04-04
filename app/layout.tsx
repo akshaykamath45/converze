391a0fbc1a81115dbe9ddcbeed65b504311ad30d
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-04-05 01:58:43 +0530

    feat: creating a meeting using stream

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
       appearance={{
         layout: {
           logoImageUrl: "/icons/logo.svg",
-          socialButtonsVariant: 'iconButton',
+          socialButtonsVariant: "iconButton",
         },
         variables: {
           colorText: "#fff",
@@ -32,7 +33,9 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+        <body className={`${inter.className} bg-dark-2`}>
+          {children} <Toaster />
+        </body>
       </html>
     </ClerkProvider>
   );
