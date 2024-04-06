7d623c75af4c9205092ab375915df0d17b2683dc
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-04-06 11:37:55 +0530

    feat: implementing meeting calls setup

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import React from "react";
-
+import { useState } from "react";
+import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
+import { useGetCallyById } from "@/hooks/useGetCallById";
+import { useParams } from "next/navigation";
 const Meeting = ({ params }: { params: { id: string } }) => {
-  return <div>Meeting Room #{params.id}</div>;
+  const {id}=useParams();
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallyById(id);
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default Meeting;
