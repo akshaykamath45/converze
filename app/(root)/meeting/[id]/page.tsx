34969aa29eddf5f01e7eed565c54f27ccdc86bcb
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-04-06 12:10:21 +0530

    feat: implementing meeting setup

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -7,16 +7,22 @@ import MeetingSetup from "@/components/MeetingSetup";
 import MeetingRoom from "@/components/MeetingRoom";
 import { useGetCallyById } from "@/hooks/useGetCallById";
 import { useParams } from "next/navigation";
+import Loader from "@/components/Loader";
 const Meeting = ({ params }: { params: { id: string } }) => {
-  const {id}=useParams();
+  const { id } = useParams();
   const { user, isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
   const { call, isCallLoading } = useGetCallyById(id);
+  if (!isLoaded || isCallLoading) return <Loader />;
   return (
     <main className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
-          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
         </StreamTheme>
       </StreamCall>
     </main>
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MeetingRoom = () => {
-  return <div></div>;
+  return <div>Meeting Room</div>;
 };
 
 export default MeetingRoom;
